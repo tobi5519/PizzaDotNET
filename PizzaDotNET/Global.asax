502efeaa5dd285dfc3ec73223324cf6741875ff7
Tobias Kith Thomsen <tobi5519@stud.kea.dk>
2018-12-30 11:35:13 +0100

    Add project files.

---
+++ PizzaDotNET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PizzaDotNET.MvcApplication" Language="C#" %>
---
+++ PizzaDotNET/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PizzaDotNET.Startup))]
+namespace PizzaDotNET
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
