ad374082916b39d762e4ce4f0daef1ea9e149800
Tobias Kith Thomsen <tobi5519@stud.kea.dk>
2018-12-31 20:31:20 +0100

    Add Unique name constrain on create pizza

---
+++ PizzaDotNET/CustomValidators/UniqueName.cs
@@ -0,0 +1,43 @@
+﻿using PizzaDotNET.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace PizzaDotNET.CustomValidators
+{
+    public class UniqueName : ValidationAttribute
+    {
+
+        private ApplicationDbContext _context;
+
+        public UniqueName()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public override bool IsValid(object value)
+        {
+            bool isValid = true;
+
+            string name = Convert.ToString(value);
+
+            var pizzaer = _context.Pizzas.ToList();
+
+            foreach (Pizza pizza in pizzaer)
+            {
+                // !string.Equals(pizza.Name, "value", StringComparison.OrdinalIgnoreCase)
+                if (pizza.Name.ToLower().Equals(name.ToLower()))
+                {
+                    return false;
+                }
+            }
+           
+
+            
+
+            return isValid;
+        }
+
+    }
+
+}
